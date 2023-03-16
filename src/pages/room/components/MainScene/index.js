4518017af7c80b6d19eea39723c19f97f0bedf4f
InvoZone <invozone@MP15-SarmadA-SE-206.local>
2023-03-16 17:09:46 +0500

    VR model added and calling tested.

--- src/pages/room/components/MainScene/index.js
+++ src/pages/room/components/MainScene/index.js
@@ -1,58 +1,220 @@
-import React, { Component } from "react";
+import React, { useRef, useEffect } from "react";
+
+// import "./style.css";
 import * as THREE from "three";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import * as dat from "dat.gui";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
-class GLTFScene extends Component {
-  constructor(props) {
-    super(props);
+const MainScene = () => {
+  useEffect(() => {
+    /**
+     * Loaders
+     */
+    const gltfLoader = new GLTFLoader();
+    const cubeTextureLoader = new THREE.CubeTextureLoader();
 
-    this.scene = new THREE.Scene();
-    this.camera = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
-    );
-    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    /**
+     * Base
+     */
+    // Debug
+    const gui = new dat.GUI();
+    const debugObject = {};
 
-    this.state = {
-      model: null,
+    // Canvas
+    const canvas = document.querySelector("canvas.webgl");
+
+    // Scene
+    const scene = new THREE.Scene();
+
+    /**
+     * Update all materials
+     */
+    const updateAllMaterials = () => {
+      scene.traverse((child) => {
+        if (
+          child instanceof THREE.Mesh &&
+          child.material instanceof THREE.MeshStandardMaterial
+        ) {
+          // child.material.envMap = environmentMap
+          child.material.envMapIntensity = debugObject.envMapIntensity;
+          child.material.needsUpdate = true;
+          child.castShadow = true;
+          child.receiveShadow = true;
+        }
+      });
     };
-  }
 
-  componentDidMount() {
-    // Add renderer to DOM
-    this.mount.appendChild(this.renderer.domElement);
+    /**
+     * Environment map
+     */
+    const environmentMap = cubeTextureLoader.load([
+      "/textures/environmentMaps/1/px.jpg",
+      "/textures/environmentMaps/1/nx.jpg",
+      "/textures/environmentMaps/1/py.jpg",
+      "/textures/environmentMaps/1/ny.jpg",
+      "/textures/environmentMaps/1/pz.jpg",
+      "/textures/environmentMaps/1/nz.jpg",
+    ]);
 
-    // Load gltf model
-    const loader = new GLTFLoader();
-    loader.load("/models/classroom/scene.gltf", (gltf) => {
-      this.scene.add(gltf.scene);
-      this.setState({ model: gltf.scene });
+    environmentMap.encoding = THREE.sRGBEncoding;
+
+    scene.background = environmentMap;
+    scene.environment = environmentMap;
+
+    debugObject.envMapIntensity = 5;
+    gui
+      .add(debugObject, "envMapIntensity")
+      .min(0)
+      .max(10)
+      .step(0.001)
+      .onChange(updateAllMaterials);
+
+    /**
+     * Models
+     */
+    // gltfLoader.load("/models/class2/gltfmodel/scene.gltf", (gltf) => {
+    //   gltf.scene.scale.set(10, 10, 10);
+    //   gltf.scene.position.set(0, -4, 0);
+    //   gltf.scene.rotation.y = Math.PI * 0.5;
+    //   scene.add(gltf.scene);
+    // });
+
+    gltfLoader.load("/models/class2/glbmodel/source/audi.glb", (gltf) => {
+      gltf.scene.scale.set(1, 1, 1);
+      gltf.scene.position.set(0, -1, 0);
+      scene.add(gltf.scene);
+
+      updateAllMaterials();
     });
 
-    // Set up camera position
-    this.camera.position.z = 5;
+    /**
+     * Lights
+     */
+    const directionalLight = new THREE.DirectionalLight("#ffffff", 3);
+    directionalLight.castShadow = true;
+    directionalLight.shadow.camera.far = 15;
+    directionalLight.shadow.mapSize.set(1024, 1024);
+    directionalLight.shadow.normalBias = 0.05;
+    directionalLight.position.set(0.25, 3, -2.25);
+    scene.add(directionalLight);
 
-    // Start animation loop
-    this.animate();
-  }
+    gui
+      .add(directionalLight, "intensity")
+      .min(0)
+      .max(10)
+      .step(0.001)
+      .name("lightIntensity");
+    gui
+      .add(directionalLight.position, "x")
+      .min(-5)
+      .max(5)
+      .step(0.001)
+      .name("lightX");
+    gui
+      .add(directionalLight.position, "y")
+      .min(-5)
+      .max(5)
+      .step(0.001)
+      .name("lightY");
+    gui
+      .add(directionalLight.position, "z")
+      .min(-5)
+      .max(5)
+      .step(0.001)
+      .name("lightZ");
 
-  animate = () => {
-    requestAnimationFrame(this.animate);
+    /**
+     * Sizes
+     */
+    const sizes = {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
 
-    if (this.state.model) {
-      // Rotate the model
-      this.state.model.rotation.y += 0.01;
-    }
+    window.addEventListener("resize", () => {
+      // Update sizes
+      sizes.width = window.innerWidth;
+      sizes.height = window.innerHeight;
 
-    // Render the scene
-    this.renderer.render(this.scene, this.camera);
-  };
+      // Update camera
+      camera.aspect = sizes.width / sizes.height;
+      camera.updateProjectionMatrix();
 
-  render() {
-    return <div ref={(mount) => (this.mount = mount)} />;
-  }
-}
+      // Update renderer
+      renderer.setSize(sizes.width, sizes.height);
+      renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    });
 
-export default GLTFScene;
+    /**
+     * Camera
+     */
+    // Base camera
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      sizes.width / sizes.height,
+      0.1,
+      100
+    );
+    camera.position.set(4, 1, -4);
+    scene.add(camera);
+
+    // Controls
+    const controls = new OrbitControls(camera, canvas);
+    controls.enableDamping = true;
+
+    /**
+     * Renderer
+     */
+    const renderer = new THREE.WebGLRenderer({
+      canvas: canvas,
+      antialias: true,
+    });
+    renderer.physicallyCorrectLights = true;
+    renderer.outputEncoding = THREE.sRGBEncoding;
+    renderer.toneMapping = THREE.ReinhardToneMapping;
+    renderer.toneMappingExposure = 3;
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+    gui
+      .add(renderer, "toneMapping", {
+        No: THREE.NoToneMapping,
+        Linear: THREE.LinearToneMapping,
+        Reinhard: THREE.ReinhardToneMapping,
+        Cineon: THREE.CineonToneMapping,
+        ACESFilmic: THREE.ACESFilmicToneMapping,
+      })
+      .onFinishChange(() => {
+        renderer.toneMapping = Number(renderer.toneMapping);
+        updateAllMaterials();
+      });
+    gui.add(renderer, "toneMappingExposure").min(0).max(10).step(0.001);
+
+    /**
+     * Animate
+     */
+    const tick = () => {
+      // Update controls
+      controls.update();
+
+      // Render
+      renderer.render(scene, camera);
+
+      // Call tick again on the next frame
+      window.requestAnimationFrame(tick);
+    };
+
+    tick();
+  }, []);
+
+  return (
+    <>
+      <canvas class="webgl"></canvas>
+    </>
+  );
+};
+
+export default MainScene;
